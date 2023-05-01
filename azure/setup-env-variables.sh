4a2702fbde31b56e058abd53efa2759c010525f6
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-05-01 19:55:09 +0200

    Update setup-env-variables.sh

--- azure/setup-env-variables.sh
+++ azure/setup-env-variables.sh
@@ -1,4 +1,3 @@
-export SUBSCRIPTION=567acf1f-09f5-48f4-bffa-48dd2e40f84c                  #  replace it with your subscription-id
 export RESOURCE_GROUP=acme-fitness-store-rg-group                         # existing resource group or one that will be created in next steps
 export SPRING_APPS_SERVICE=sshtechspring001                 # name of the service that will be created in the next steps
 export LOG_ANALYTICS_WORKSPACE=sshtechloganaly   # existing workspace or one that will be created in next steps
