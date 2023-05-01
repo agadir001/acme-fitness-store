9bf0c3592b2af956843b4e51f17ffe9a7758af6c
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-05-01 15:25:54 +0200

    Create setup-env-variables.sh

---
+++ azure/templates/setup-env-variables.sh
@@ -0,0 +1,14 @@
+export SUBSCRIPTION=567acf1f-09f5-48f4-bffa-48dd2e40f84c                  #  replace it with your subscription-id
+export RESOURCE_GROUP=acme-fitness-store-rg-group                         # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=sshtechspring001                 # name of the service that will be created in the next steps
+export LOG_ANALYTICS_WORKSPACE=sshtechloganaly   # existing workspace or one that will be created in next steps
+export REGION=east                           # choose a region with Enterprise tier support
+
+export CART_SERVICE_APP="cart-service"
+export IDENTITY_SERVICE_APP="identity-service"
+export ORDER_SERVICE_APP="order-service"
+export PAYMENT_SERVICE_APP="payment-service"
+export CATALOG_SERVICE_APP="catalog-service"
+export FRONTEND_APP="frontend"
+
+export CUSTOM_BUILDER="no-bindings-builder"
